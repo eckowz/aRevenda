718b66632ef3fa6578a7c93ca7e46e60f7f2dadf
Eco <guilherme_eco@hotmail.com>
2016-05-03 11:24:32 -0300

    Inclusao de venda, em construcao.

--- MinhaRevenda/src/java/bean/VendaMB.java
+++ MinhaRevenda/src/java/bean/VendaMB.java
@@ -5,10 +5,68 @@
  */
 package bean;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ApplicationScoped;
+import javax.inject.Named;
+import model.Venda;
+
 /**
  *
  * @author gserafini
  */
-public class VendaMB {
+@Named
+@ApplicationScoped
+public class VendaMB implements Serializable{
     
+    private List<Venda> listaVendas;
+    private Venda vendaSelecionada;
+    VeiculoMB veiculoMB = new VeiculoMB();
+    UsuarioMB usuarioMB = new UsuarioMB();
+    
+    public VendaMB() {
+        vendaSelecionada = new Venda();
+        listaVendas = new ArrayList<Venda>();
+        
+    }
+            
+    public Venda getVendaSelecionada() {
+        return vendaSelecionada;
+    }
+
+    public void setVendaSelecionado(Venda vendaSelecionada) {
+        this.vendaSelecionada = vendaSelecionada;
+    }
+
+    public List<Venda> getListaVendas() {
+        return listaVendas;
+    }
+
+    public void setListaVendas(List<Venda> listaVendas) {
+        this.listaVendas = listaVendas;
+    }
+
+    public String novoVenda() {
+        vendaSelecionada = new Venda();
+        return ("/comum/formCadastroVenda?faces-redirect=true");
+    }
+
+    public String addVendas() {
+        listaVendas.add(vendaSelecionada);
+        return ("/comum/formVenda?faces-redirect=true");
+    }
+
+    public void removerVenda(Venda venda) {
+        listaVendas.remove(venda);
+    }
+
+        public Venda buscarVenda(int codVenda) {
+        for (Venda venda : listaVendas) {
+            if (venda.getCodigo() == (codVenda)) {
+                return venda;
+            }
+        }
+        return null;
+    }    
 }
--- MinhaRevenda/src/java/model/Venda.java
+++ MinhaRevenda/src/java/model/Venda.java
@@ -5,10 +5,97 @@
  */
 package model;
 
+import java.util.Objects;
+
 /**
  *
  * @author gserafini
  */
 public class Venda {
+    private static final long serialVersionUID = 1L;
+    private int codigo;
+    private Veiculo veiculo;
+    private String cliente;
+    private Usuario usuario;
+
+    public Venda() {
+    }
+
+    public Venda(int codigo, Veiculo veiculo, String cliente, Usuario usuario) {
+        this.codigo = codigo;
+        this.veiculo = veiculo;
+        this.cliente = cliente;
+        this.usuario = usuario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + this.codigo;
+        hash = 53 * hash + Objects.hashCode(this.veiculo);
+        hash = 53 * hash + Objects.hashCode(this.cliente);
+        hash = 53 * hash + Objects.hashCode(this.usuario);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Venda other = (Venda) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        if (!Objects.equals(this.cliente, other.cliente)) {
+            return false;
+        }
+        if (!Objects.equals(this.veiculo, other.veiculo)) {
+            return false;
+        }
+        if (!Objects.equals(this.usuario, other.usuario)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
